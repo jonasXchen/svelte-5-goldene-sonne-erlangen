adf5d0e0b7b2005e31632551576aa89250ec0d6b
jonasXchen <jonas@metacamp.so>
2024-09-06 16:48:26 +0800

    Goldene Sonne Erlangen

---
+++ src/pages/Directions.svelte
@@ -0,0 +1,52 @@
+<script>
+  import {
+    PUBLIC_RESTAURANT_LUNCH_TIME,
+    PUBLIC_RESTAURANT_DINNER_TIME,
+    PUBLIC_RESTAURANT_OPENING_DAYS,
+  } from "$lib/constants";
+
+  import DirectionsImage from "/src/assets/images/directions.png";
+</script>
+
+<div class="flex flex-col lg:px-8 md:px-20 sm:px-16 p-8 gap-8 w-full">
+  <div class="flex w-full">
+    <p
+      class="w-full font-cinzel text-primary text-3xl font-medium text-center sm:text-left tracking-widest"
+    >
+      So finden Sie uns!
+    </p>
+  </div>
+  <p class="text-balance text-center sm:text-left">
+    <span class="font-bold">{PUBLIC_RESTAURANT_OPENING_DAYS}</span>, auch an
+    <span class="font-bold">Feiertagen</span>
+    ab
+    <span class="font-bold">{PUBLIC_RESTAURANT_LUNCH_TIME}</span> &
+    <span class="font-bold">{PUBLIC_RESTAURANT_DINNER_TIME}</span> sind wir für Sie
+    da!
+  </p>
+  <div class="flex lg:flex-row flex-col lg:gap-12 gap-8">
+    <div class="flex flex-col gap-4 w-full">
+      <!-- Map -->
+      <div class="flex w-full sm:h-[320px] md:h-[400px] h-[240px]">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10345.265272991406!2d11.0051418!3d49.5917841!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a1f9c1c206c3f3%3A0x5aa647340fbc428b!2sChina%20Restaurant%20Goldene%20Sonne!5e0!3m2!1sde!2ssg!4v1725606987201!5m2!1sde!2ssg"
+          width="100%"
+          height="100%"
+          style="border:0;"
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+    </div>
+    <div class="flex flex-col gap-4 w-full">
+      <img
+        src={DirectionsImage}
+        class="md:h-[360px] sm:h-[280px] object-cover h-[200px] w-full"
+      />
+      <p class="text-balance text-center sm:text-left">
+        Ausreichend Parkplätze direkt unter dem Einkaufszentrum und Restaurant.
+      </p>
+    </div>
+  </div>
+</div>
---
+++ src/pages/Facilities.svelte
@@ -0,0 +1,57 @@
+<script>
+  import ImageCarousel from "../components/ImageCarousel.svelte";
+
+  import EntranceImage from "/src/assets/images/entrance.png";
+  import Facilities2Image from "/src/assets/images/facilities2.png";
+  import TablesImage from "/src/assets/images/tables.png";
+  import Facilities3Image from "/src/assets/images/facilities3.png";
+
+  let images = [
+    {
+      file: EntranceImage,
+      name: "Herzlich Willkommen!",
+    },
+    {
+      file: Facilities2Image,
+      name: "Elegant traditionell",
+    },
+    {
+      file: TablesImage,
+      name: "Gemeinsam und gemütlich",
+    },
+    {
+      file: Facilities3Image,
+      name: "Viel Platz",
+    },
+  ];
+</script>
+
+<div class="flex flex-col lg:px-8 md:px-20 sm:px-16 p-8 gap-8">
+  <!-- Title -->
+  <div
+    class="flex font-cinzel text-primary text-3xl font-medium tracking-widest leading-10"
+  >
+    Unsere Räumlichkeiten
+  </div>
+  <!-- Fotos -->
+  <div class="flex relative">
+    <!-- <div
+      class="absolute top-0 left-0 bg-white [clip-path:polygon(0%_0%,100%_0%,100%_60%,0%_30%)] w-full h-1/5 opacity-35 z-20"
+    ></div>
+    <div
+      class="absolute top-0 left-0 bg-white [clip-path:polygon(0%_0%,100%_0%,100%_60%,0%_30%)] w-full h-1/6 opacity-100 z-20"
+    ></div> -->
+    <ImageCarousel
+      carouselClass="sm:h-[340px] lg:h-[640px] md:h-[440px] h-[280px] max-h-[620px] rounded-xl gap-0"
+      imageClass="object-cover rounded-xl"
+      imageTitle={true}
+      {images}
+    />
+    <!-- <div
+      class="absolute bottom-0 left-0 bg-white [clip-path:polygon(0%_0%,100%_50%,100%_100%,0%_100%)] w-full h-1/5 opacity-35"
+    ></div>
+    <div
+      class="absolute bottom-0 left-0 bg-white [clip-path:polygon(0%_0%,100%_50%,100%_100%,0%_100%)] w-full h-1/6 opacity-100"
+    ></div> -->
+  </div>
+</div>
