7167bd16ee86194061c56812241d7a66eb1de5e2
jonasXchen <jonas002@hotmail.de>
2026-05-05 09:18:50 +0800

    feat: baustelle

--- src/pages/Directions.svelte
+++ src/pages/Directions.svelte
@@ -32,6 +32,7 @@
           width="100%"
           height="100%"
           style="border:0;"
+          allowfullscreen=""
           loading="lazy"
           referrerpolicy="no-referrer-when-downgrade"
         ></iframe>
